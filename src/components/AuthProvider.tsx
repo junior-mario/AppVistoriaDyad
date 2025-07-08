8af97448c360d9abdd5dfbb780daf5a52a8001c3
Mario A Franco Jr <srqbtr87@srqb.local>
2025-07-08 14:29:32 -0300

    Reverted all changes back to version 2b17ebb1889ff41ccd4b529623514c09219e04e1

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,15 +1,10 @@
 "use client"
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [supabaseClient] = useState(() => 
-    createClientComponentClient({
-      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    })
-  )
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
   
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
